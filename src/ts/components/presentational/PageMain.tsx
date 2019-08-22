4ee6a2481d800fa5d7ed7bde5f91c1750f7a0b7c
richifg <richifg@gmail.com>
2019-08-22 10:06:36 -0600

    fixed vertical scroll

--- src/ts/components/presentational/PageMain.tsx
+++ src/ts/components/presentational/PageMain.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 
 // A scrollable container where page content is displayed
 const PageMain: React.FunctionComponent = ({ children }) => (
-  <main className="main">
-    {children}
+  <main className="main px-2">
+      <div className="scroll-y">
+        {children}
+      </div>
   </main>
 );
 
