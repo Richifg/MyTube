b1df765e1f3e28b136eb6baf5e67df910aa83e44
richifg <richifg@gmail.com>
2019-08-23 17:14:51 -0600

    made loadMoreButton visible only showing videos

--- src/ts/pages/SearchPage.tsx
+++ src/ts/pages/SearchPage.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { Container, Row } from 'reactstrap';
+import { observer, inject } from 'mobx-react';
 
 import PageHeader from '../components/presentational/PageHeader';
 import PageMain from '../components/presentational/PageMain';
-import SearchBar from '../components/container/SearchBar';
+import SearchBar from '../components/presentational/SearchBar';
 import VideoPreviewList from '../components/container/VideoPreviewList';
 import LoadMoreButton from '../components/presentational/ButtonLoadMore';
+import { IYoutube } from '../interfaces';
 
-const MainPage = () => (
+const MainPage = inject('youtube')(observer(({ youtube }: IYoutube) => (
   <React.Fragment>
     <PageHeader />
     <PageMain>
@@ -16,12 +18,14 @@ const MainPage = () => (
           <SearchBar />
         </Row>
         <VideoPreviewList />
-        <Row className="justify-content-center my-4">
-          <LoadMoreButton />
-        </Row>
+        { !!youtube.searchVideos.length && (
+          <Row className="justify-content-center my-4">
+            <LoadMoreButton />
+          </Row>
+        )}
       </Container>
     </PageMain>
   </React.Fragment>
-);
+)));
 
 export default MainPage;
