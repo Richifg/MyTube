1d699a2df1fe187a8393c5ed8a5bd457d26691fb
richifg <richifg@gmail.com>
2019-08-22 22:41:04 -0600

    refactored youtubeStore

--- src/ts/stores/YoutubeStore.ts
+++ src/ts/stores/YoutubeStore.ts
@@ -1,7 +1,6 @@
 import { observable, autorun } from 'mobx';
 
-const videoSearchFields = 'nextPageToken,items(etag,snippet(title,description,thumbnails(medium(url))))';
-
+// interfaces
 interface IItems {
   etag: string;
   snippet: {
@@ -11,13 +10,29 @@ interface IItems {
     thumbnails: { medium: { url: string}}
   };
 }
-
 interface IVideo {
   title: string;
   description: string;
   img: string;
   id?: string;
 }
+
+// helper functions
+const searchOptions = (query: string) => ({
+  part: 'snippet',
+    maxResults: 25,
+    type: 'video',
+    q: query,
+    fields: 'nextPageToken,items(etag,snippet(title,description,thumbnails(medium(url))))',
+});
+const searchNextOptions = (query: string, pageToken: string ) => (
+  Object.assign({}, searchOptions(query), { pageToken })
+);
+const truncateString = (str: string, maxLength: number) => {
+  return (str.length > maxLength
+  ? `${str.substr(0, 122)}...`
+  : str);
+};
 
 class YoutubeStore {
   @observable searchVideos: IVideo[];
@@ -34,7 +49,7 @@ class YoutubeStore {
     const body = JSON.parse(response.body);
     const newVideos = body.items.map((item: IItems) => ({
       title: item.snippet.title,
-      description: item.snippet.description,
+      description: truncateString(item.snippet.description, 125),
       img: item.snippet.thumbnails.medium.url,
       id: item.etag,
     }));
@@ -46,17 +61,13 @@ class YoutubeStore {
     this.nextPage = body.nextPageToken;
   }
 
-  /* actions */
+  // actions
   search = () => {
+    // ##DEBUG
+    console.log('opening list request for search...');
     if (this.query) {
       // could't find working types for youtube
-      (gapi.client as any).youtube.search.list({
-        part: 'snippet',
-          maxResults: 1,
-          q: this.query,
-          type: 'video',
-          fields: videoSearchFields,
-      })
+      (gapi.client as any).youtube.search.list(searchOptions(this.query))
         .then(this.handleSearchResponse);
     } else {
       this.searchVideos = [];
@@ -65,14 +76,9 @@ class YoutubeStore {
   }
 
   searchNext = () => {
-    (gapi.client as any).youtube.search.list({
-      part: 'snippet',
-        maxResults: 1,
-        q: this.query,
-        type: 'video',
-        fields: videoSearchFields,
-        pageToken: this.nextPage,
-    })
+    // ##DEBUG
+    console.log('opening list request for next search...');
+    (gapi.client as any).youtube.search.list(searchNextOptions(this.query, this.nextPage))
       .then((response: any) => this.handleSearchResponse(response, true));
   }
 
