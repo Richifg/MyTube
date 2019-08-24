fd0fe3bf941d974d33aaa8da1625cb6557f496e8
richifg <richifg@gmail.com>
2019-08-23 18:15:04 -0600

    added youtube iframe api

--- src/ts/components/container/App.tsx
+++ src/ts/components/container/App.tsx
@@ -18,38 +18,47 @@ import { APIkey } from '../../../../APIkey';
 @observer
 class App extends React.Component {
   @observable isGapiReady: boolean;
+  @observable isIframeReady: boolean;
   youtubeStore: YoutubeStore;
   favoritesStore: FavoritesStore;
 
   constructor(props: any) {
     super(props);
     this.isGapiReady = false;
+    this.isIframeReady = false;
     this.youtubeStore = new YoutubeStore();
     this.favoritesStore = new FavoritesStore();
   }
 
   componentDidMount() {
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/client.js';
-    script.onload = () => this.initGapi(script);
-    document.head.appendChild(script);
+    // Initialize both APIs
+
+    const scriptIframe = document.createElement('script');
+    scriptIframe.src = 'https://www.youtube.com/iframe_api';
+    // this function is called by the API automatically when its code downloads
+    (window as any).onYouTubeIframeAPIReady = () => this.isIframeReady = true;
+
+    const scriptGapi = document.createElement('script');
+    scriptGapi.src = 'https://apis.google.com/js/client.js';
+    // gapi only sets a flag in the script tag so have to check for it manually
+    scriptGapi.onload = () => this.initGapi(scriptGapi);
+
+    document.head.appendChild(scriptIframe);
+    document.head.appendChild(scriptGapi);
   }
 
   initGapi = (script: HTMLScriptElement) => {
-    /*
-      gapi is not made available inmediatly after the script loads
-    */
-    if (!script.getAttribute('gapi_processed')) {
-      setTimeout(() => this.initGapi(script), 50);
-      return;
-    }
-    gapi.client.setApiKey(APIkey);
-    gapi.client.load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest', 'v3')
-      .then(() => { this.isGapiReady = true; });
+  if (!script.getAttribute('gapi_processed')) {
+    setTimeout(() => this.initGapi(script), 50);
+    return;
+  }
+  gapi.client.setApiKey(APIkey);
+  gapi.client.load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest', 'v3')
+    .then(() => { this.isGapiReady = true; });
   }
 
   render() {
-    if (this.isGapiReady) {
+    if (this.isGapiReady && this.isIframeReady) {
       return (
         <Provider youtube={this.youtubeStore} favorites={this.favoritesStore}>
           <Router>
@@ -61,7 +70,7 @@ class App extends React.Component {
         </Provider>
       );
     }
-    return ( <LoadingSpinner message="Loading Google API..."/> );
+    return ( <LoadingSpinner message="Initializing Google APIs..."/> );
   }
 }
 
