b5d7f254b48d5c83d38a19202d2bbe1ed0b3417e
richifg <richifg@gmail.com>
2019-08-25 21:25:51 -0600

    added autoplay feature

--- src/ts/components/presentational/VideoPlayer.tsx
+++ src/ts/components/presentational/VideoPlayer.tsx
@@ -6,9 +6,10 @@ const VideoPlayer = ({ videoId }: IVideoPlayer) => (
   <div className="video-ratio">
     <iframe
       className="video-player border-bottom border-dark"
-      src={`https://www.youtube.com/embed/${videoId}?&allowfullscreeen=1`}
+      src={`https://www.youtube.com/embed/${videoId}?&allowfullscreeen=1&autoplay=1`}
       frameBorder={0}
       allowFullScreen
+      allow="autoplay"
     />
   </div>
 );
